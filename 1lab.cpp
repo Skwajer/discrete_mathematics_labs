e015f2fe1a9dcb1ac2059c395c668282607603d9
skwajer <skwajergg@gmail.com>
2025-11-09 14:11:24 +0300

    fixed add method and added see[set] / see(all_sets)

--- 1lab.cpp
+++ 1lab.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <iostream>
 #include <stdexcept>
 #include <vector>
 
@@ -8,6 +9,7 @@ struct Node
     Node *next;
 
     Node(char value) : data(value), next(nullptr) {} 
+
 };
 
 class Set
@@ -64,7 +66,7 @@ class Set
     {
         if (head == nullptr)
         {
-            std::logic_error("first, create a set");
+            throw std::logic_error("first, create a set");
         }
 
         Node *new_node = new Node(value);
@@ -75,8 +77,16 @@ class Set
             prev_iter = curr_iter;
             curr_iter = curr_iter->next;
         }
+
+        if (curr_iter && value == curr_iter->data)
+        {
+            delete new_node;
+            return;
+        }
+
         if (prev_iter == nullptr)
         {
+            new_node->next = head->next;
             head->next = new_node;
             return;
         }
@@ -84,59 +94,75 @@ class Set
         new_node->next = curr_iter;
     }
 
-    void del(char value)
-    {
-        if (head == nullptr)
-        {
-            std::invalid_argument("first, create a set");
-        }
-        if (head->next == nullptr)
-        {
-            return;
-        }
+    // void del(char value)
+    // {
+    //     if (head == nullptr)
+    //     {
+    //         std::invalid_argument("first, create a set");
+    //     }
+    //     if (head->next == nullptr)
+    //     {
+    //         return;
+    //     }
 
-        Node *temp_ptr;
-        Node *curr_iter = head->next;
-        Node *prev_iter = nullptr;
-        // if (value == curr_iter->data)
-        // {
+    //     Node *temp_ptr;
+    //     Node *curr_iter = head->next;
+    //     Node *prev_iter = nullptr;
+    //     // if (value == curr_iter->data)
+    //     // {
 
-        // }
-        while(curr_iter != nullptr && curr_iter->data != value)
-        {
-            prev_iter = curr_iter;
-            curr_iter = curr_iter->next;
-        }
-        if (curr_iter != nullptr)
-        {
-            temp_ptr = curr_iter;
-            prev_iter->next = curr_iter->next;
+    //     // }
+    //     while(curr_iter != nullptr && curr_iter->data != value)
+    //     {
+    //         prev_iter = curr_iter;
+    //         curr_iter = curr_iter->next;
+    //     }
+    //     if (curr_iter != nullptr)
+    //     {
+    //         temp_ptr = curr_iter;
+    //         prev_iter->next = curr_iter->next;
             
-        }        
-    }
+    //     }        
+    // }
 
     static void see()
     {
-        
+        if (all_sets.size() == 0)
+        {
+            throw std::logic_error("not a single set has been created yet");
+        }
+
+        for (Set *set : all_sets)
+        {
+            set->see(set->head->data);
+        }
     }
 
-    static void see(char set_name)
+    void see(char set_name) const
     {
         if (find_set(set_name) == nullptr)
         {
-            throw std::invalid_argument("set not exist");
+            throw std::invalid_argument("set is not exist");
         }
 
-        
+        printf("elements of %c set: ", head->data);
+        Node *iter = head->next;
+        while (iter)
+        {
+            printf("%c ", iter->data);
+            iter = iter->next;
+        }
+        std::cout << std::endl;
     }
 
 };
 
-std::vector<Set*> Set::all_sets;
+std::vector<Set*> Set::all_sets = {};
 
 int main()
 {
     Set A('A');
+    Set C('C');
     printf("set %c was created\n", A.get_name());
 
     try 
@@ -150,6 +176,16 @@ int main()
     A.add('a');
     A.add('c');
     A.add('b');
+
+    C.add('a');
+    C.add('g');
+    C.add('c');
+    C.add('J');
+    C.add('Z');
+    C.add('g');
+    C.add('#');
+    Set::see();
+
 
     return 0;
 }
